e4b3bb7c97a2513b52c09410967e39cf72753cc7
bcarls <bcarls@gmail.com>
2014-11-12 16:13:46 -0600

    Adding a button for the cathode as well, to turn it on/off

--- Assets/buttonTest.js
+++ Assets/buttonTest.js
@@ -8,8 +8,6 @@ function OnGUI () {
 
     // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
     if (GUI.Button (Rect (20,40,80,20), "Cryostat")) {
-        //Application.LoadLevel (1);
-        //GetComponent(MeshRenderer).enabled = false;
         for(var child : Transform  in transform)
         {
           if(child.name == "Cryostat_001" || child.name == "Endcap"){
@@ -19,11 +17,18 @@ function OnGUI () {
               child.GetComponent(MeshRenderer).enabled = true;
           }
         }
-        
     }
 
     // Make the second button.
-    if (GUI.Button (Rect (20,70,80,20), "Level 2")) {
-        //Application.LoadLevel (2);
+    if (GUI.Button (Rect (20,70,80,20), "Cathode")) {
+        for(var child : Transform  in transform)
+        {
+          if(child.name == "Cathode"){
+            if(child.GetComponent(MeshRenderer).enabled == true)
+              child.GetComponent(MeshRenderer).enabled = false;
+            else  
+              child.GetComponent(MeshRenderer).enabled = true;
+          }
+        } 
     }
 }
